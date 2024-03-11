2ae09ba0d6ed671668be8a7fca49d508fdee9ee4
arcadio <rkdo.skandari@gmail.com>
2024-03-11 20:52:43 +0330

    add page

---
+++ src/app/cookies/page.tsx
@@ -0,0 +1,20 @@
+"use server";
+import React from "react";
+import { loger } from "./test";
+import { CookeisPlayGround } from "./-components/CookeisPlayGround";
+
+const page = () => {
+  return (
+    <div>
+      <CookeisPlayGround />
+      <form action={loger}>
+        <input type="text" name="name" />
+        <button className="text-lime-600" type="submit">
+          adder
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default page;
